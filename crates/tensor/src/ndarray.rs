0d1e7f33173f5145740e95a8665ff0b3f2255f48
nextdorf <nextdorf@users.noreply.github.com>
2023-02-05 22:00:27 +0100

    ndarray

--- crates/tensor/src/ndarray.rs
+++ crates/tensor/src/ndarray.rs
@@ -2,59 +2,64 @@ use std::marker::PhantomData;
 
 pub use ndarray as np;
 
-use np::{IntoDimension, OwnedRepr, RawData};
+use ndarray_einsum_beta::{tensordot, einsum};
+use np::RawData;
 pub use np::prelude::*;
 
-use crate::tensorspace::{TensorAlgebra, DualVariant};
+use crate::{
+  tensorspace::{
+    TensorAlgebra,
+    DualVariant
+  },
+  move_vec_elems,
+  cycles
+};
 
 
-pub struct NDArrayAlgebraOwned<S: np::RawData>(PhantomData<S>);
+pub struct NDArrayAlgebra<A>(PhantomData<A>);
 // pub struct NDArrayAlgebra<S: np::RawData>(PhantomData<S>);
 
 
 // impl<S> TensorAlgebra for NDArrayAlgebra<S> where S: np::RawDataClone + np::Data, S::Elem: Clone + np::LinalgScalar {
 // impl<S> TensorAlgebra for NDArrayAlgebra<S> where S: np::RawData + np::RawDataClone + np::Data, S::Elem: np::LinalgScalar {
-impl<S> TensorAlgebra for NDArrayAlgebraOwned<S> where S: RawData + Clone + np::LinalgScalar {
+impl<A> TensorAlgebra for NDArrayAlgebra<A> where A: RawData + Clone + np::LinalgScalar {
   type Index = Vec<(np::Axis, usize)>;
   type Shape = Vec<np::AxisDescription>;
   // type Tensor = ArrayBase<S, IxDyn>;
-  type Tensor = ArrayBase<OwnedRepr<S>, IxDyn>;
+  type Tensor = ArrayD<A>;
   type IndexErr = ();
   type TensorErr = ();
 
-  fn swap_indices(idx: Self::Index, i: usize, j: usize) -> Result<Self::Index, Self::IndexErr> {
-    todo!()
+  fn swap_indices(mut idx: Self::Index, i: usize, j: usize) -> Result<Self::Index, Self::IndexErr> {
+    idx.swap(i, j);
+    Ok(idx)
   }
 
-  fn move_index(idx: Self::Index, src: usize, dst: usize) -> Result<Self::Index, Self::IndexErr> {
-    todo!()
+  fn move_index(mut idx: Self::Index, src: usize, dst: usize) -> Result<Self::Index, Self::IndexErr> {
+    move_vec_elems(&mut idx, src, dst);
+    Ok(idx)
   }
 
   fn shape_variant(idx: Self::Shape, i: usize) -> Option<DualVariant> {
-    todo!()
+    if i < idx.len() {
+      Some(DualVariant::Contra)
+    } else {
+      None
+    }
   }
 
-  fn swap_shape_indices(idx: Self::Shape, i: usize, j: usize) -> Result<Self::Shape, Self::IndexErr> {
-    todo!()
+  fn swap_shape_indices(mut idx: Self::Shape, i: usize, j: usize) -> Result<Self::Shape, Self::IndexErr> {
+    idx.swap(i, j);
+    Ok(idx)
   }
 
-  fn move_shape_index(idx: Self::Shape, src: usize, dst: usize) -> Result<Self::Shape, Self::IndexErr> {
-      todo!()
+  fn move_shape_index(mut idx: Self::Shape, src: usize, dst: usize) -> Result<Self::Shape, Self::IndexErr> {
+    move_vec_elems(&mut idx, src, dst);
+    Ok(idx)
   }
 
-  fn mul(a: Self::Tensor, b: Self::Tensor) -> Self::Tensor {
-    let a_shape = a.shape();
-    let a_len = a.len();
-    let b_shape = b.shape();
-    let b_len = b.len();
-    let mut shape = Vec::with_capacity(a_shape.len() + b_shape.len());
-    shape.append(&mut Vec::from(a_shape));
-    shape.append(&mut Vec::from(b_shape));
-
-    let a_flat: ArrayBase<_, Dim<[usize; 2]>> = a.into_shape(Ix2(a_len, 1)).unwrap();
-    let b_flat: ArrayBase<_, Dim<[usize; 2]>> = b.into_shape(Ix2(1, b_len)).unwrap();
-    let c_mat = a_flat.dot(&b_flat);
-    c_mat.into_shape(shape).unwrap()
+  fn mul(a: ArrayD<A>, b: ArrayD<A>) -> ArrayD<A> {
+    NDArrayAlgebra::contractions_over(&a, &b, &[])
   }
 
   fn rank(t: &Self::Tensor) -> (usize, usize) {
@@ -108,9 +113,91 @@ impl<S> TensorAlgebra for NDArrayAlgebraOwned<S> where S: RawData + Clone + np::
     }
   }
 
-  fn contractions(t: Self::Tensor, idxs: &[(usize, usize)]) -> Result<Self::Tensor, Self::TensorErr> {
-    ArrayBase::
+  fn contractions_with(a: Self::Tensor, b: Self::Tensor, idxs: &[(usize, usize)]) -> Result<Self::Tensor, Self::TensorErr> {
+    Ok(NDArrayAlgebra::contractions_with(&a, &b, idxs))
   }
+  
+  fn contractions(t: Self::Tensor, idxs: &[(usize, usize)]) -> Result<Self::Tensor, Self::TensorErr> {
+    Ok(NDArrayAlgebra::contractions(&t, idxs))
+  }
+
+  fn contraction_with(a: Self::Tensor, b: Self::Tensor, i: usize, j: usize) -> Result<Self::Tensor, Self::TensorErr> {
+    Ok(NDArrayAlgebra::contraction_with(&a, &b, i, j))
+  }
+
+  fn contraction(t: Self::Tensor, i: usize, j: usize) -> Result<Self::Tensor, Self::TensorErr> {
+    Ok(NDArrayAlgebra::contraction(&t, i, j))
+  }
+
+  
 }
 
 
+
+impl<A: np::LinalgScalar> NDArrayAlgebra<A> {
+  pub fn contractions_over<RhsS, LhsS, RhsD, LhsD>(lhs: &ArrayBase<LhsS, LhsD>, rhs: &ArrayBase<RhsS, RhsD>, axes: &[(Axis, Axis)]) -> ArrayD<A>
+    where RhsS: np::Data<Elem = A>, LhsS: np::Data<Elem = A>, LhsD: Dimension, RhsD: Dimension
+  {
+    let (mut lhs_axes, mut rhs_axes) = (Vec::with_capacity(axes.len()), Vec::with_capacity(axes.len()));
+    for (a, b) in axes {
+      lhs_axes.push(*a);
+      rhs_axes.push(*b);
+    }
+    tensordot(lhs, rhs, &lhs_axes[..], &rhs_axes[..])
+  }
+  
+  pub fn contractions_with<RhsS, LhsS, RhsD, LhsD>(lhs: &ArrayBase<LhsS, LhsD>, rhs: &ArrayBase<RhsS, RhsD>, axes: &[(usize, usize)]) -> ArrayD<A>
+    where RhsS: np::Data<Elem = A>, LhsS: np::Data<Elem = A>, LhsD: Dimension, RhsD: Dimension
+  {
+    let (mut lhs_axes, mut rhs_axes) = (Vec::with_capacity(axes.len()), Vec::with_capacity(axes.len()));
+    for (a, b) in axes {
+      lhs_axes.push(Axis(*a));
+      rhs_axes.push(Axis(*b));
+    }
+    tensordot(lhs, rhs, &lhs_axes[..], &rhs_axes[..])
+  }
+  
+  pub fn contraction_with<RhsS, LhsS, RhsD, LhsD>(lhs: &ArrayBase<LhsS, LhsD>, rhs: &ArrayBase<RhsS, RhsD>, i: usize, j: usize) -> ArrayD<A>
+    where RhsS: np::Data<Elem = A>, LhsS: np::Data<Elem = A>, LhsD: Dimension, RhsD: Dimension
+  {
+    tensordot(lhs, rhs, &[Axis(i)], &[Axis(j)])
+  }
+  
+  pub fn contractions<S, D>(t: &ArrayBase<S, D>, axes: &[(usize, usize)]) -> ArrayD<A> where S: np::Data<Elem = A>, D: Dimension {
+    // const letters: &str = "abcdefghijklmnopqrstuvwxyzαβγδεζηθικλμνξοπρστυφχψω";
+    const LETTERS: [char; 26 + 24] = [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+      'u', 'v', 'w', 'x', 'y', 'z', 'α', 'β', 'γ', 'δ',
+      'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ',
+      'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ', 'ω'
+    ];
+    let max_axis_val = axes.iter().max().unwrap();
+    let max_axis_val = max_axis_val.0.max(max_axis_val.1);
+    assert!(max_axis_val < LETTERS.len());
+
+    let axes_cycles = cycles(axes);
+    let mut input_str = Vec::from_iter(std::iter::repeat(' ').take(t.len()));
+    let mut i = 0;
+    for axis_set in axes_cycles {
+      for axi in axis_set {
+        input_str[axi] = LETTERS[i];
+      }
+      i += 1
+    }
+    let mut output_str = Vec::new();
+    for c in input_str.iter_mut() {
+      if *c == ' ' {
+        *c = LETTERS[i];
+        output_str.push(LETTERS[i]);
+        i+=1;
+      }
+    }
+    let input_str = String::from_iter(input_str) + " -> " + String::from_iter(output_str).as_str();
+    einsum(input_str.as_str(), &[t]).unwrap()
+  }
+  
+  pub fn contraction<S, D>(t: &ArrayBase<S, D>, i: usize, j: usize) -> ArrayD<A> where S: np::Data<Elem = A>, D: Dimension {
+    Self::contractions(t, &[(i, j)])
+  }
+}
--- crates/tensor/src/util.rs
+++ crates/tensor/src/util.rs
@@ -1,4 +1,4 @@
-use std::iter::Skip;
+use std::{hash::Hash, collections::HashSet};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Dim<const N: usize>(pub usize);
@@ -15,28 +15,56 @@ impl<const N: usize> Dim<N> {
 }
 
 
-//use step-by instead
-// pub struct NthIter<Iter: Iterator>{
-//   i: Iter,
-//   n: usize,
-// }
 
-// impl<Iter: Iterator> Iterator for NthIter<Iter>  {
-//   type Item = Iter::Item;
+pub fn cycles<T: Eq + Copy + Hash>(edges: &[(T, T)]) -> Vec<HashSet<T>> {
+  let mut res = Vec::<HashSet<T>>::new();
+  'egde_loop: for (e1, e2) in edges {
+    for s in res.iter_mut() {
+      if s.contains(e1) {
+        s.insert(*e2);
+        continue 'egde_loop;
+      } else if s.contains(e2) {
+        s.insert(*e1);
+        continue 'egde_loop;
+      }
+    }
+    res.push(HashSet::from([*e1, *e2]));
+  }
+  res
+}
 
-//   fn next(&mut self) -> Option<Self::Item> {
-//     self.i.nth(self.n)
-//   }
-// }
+#[test]
+fn test_cycles() {
+  assert_eq!(
+    cycles::<i32>(&[]),
+    vec![],
+  );
 
-// impl<Iter: Iterator> NthIter<Iter> {
-//   pub fn from(i: Iter, n: usize) -> Self {
-//     Self { i, n }
-//   }
+  assert_eq!(
+    cycles(&[(1,2), (3,2), (4,5)]),
+    Vec::from([vec![1, 2, 3], vec![4, 5]].map(HashSet::from_iter)),
+  );
 
-//   pub fn with_offset_from(i: Iter, n: usize, offset: usize) -> NthIter<Skip<Iter>> {
-//     NthIter { i: i.skip(offset), n }
-//   }
-// }
+  assert_eq!(
+    cycles(&[(1,2), (3,2), (4,2), (5, 5), (6, 7), (7, 6), (7, 8), (8, 9)]),
+    Vec::from([vec![1, 2, 3, 4], vec![5], vec![6, 7, 8, 9]].map(HashSet::from_iter)),
+  );
+}
 
+
+pub fn move_vec_elems<T>(elems: &mut Vec<T>, src: usize, dst: usize) {
+  match src.cmp(&dst) {
+    std::cmp::Ordering::Less => {
+      for i in src..dst {
+        elems.swap(i, i+1)
+      }
+    },
+    std::cmp::Ordering::Greater => {
+      for i in ((dst+1)..=src).rev() {
+        elems.swap(i, i-1)
+      }
+    },
+    std::cmp::Ordering::Equal => (),
+  }
+}
 
