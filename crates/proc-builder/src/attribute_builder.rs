fdc7db906398dbb0a8753532380c9bcb7b33b7e1
nextdorf <nextdorf@users.noreply.github.com>
2023-05-08 19:31:15 +0200

    Worked on proc-builder

--- crates/proc-builder/src/attribute_builder.rs
+++ crates/proc-builder/src/attribute_builder.rs
@@ -1,26 +1,22 @@
-use std::{collections::HashMap, any::Any};
+use std::{collections::HashMap, any::Any, rc::Rc};
 
 use proc_macro2::TokenStream;
 use syn::{Path, Expr, Attribute, Meta, punctuated::Punctuated, Token, parse_quote};
 
-pub type DynLookup = HashMap<String, Box<dyn Any>>;
+pub struct DynLookup(HashMap<String, (Box<dyn Any>, &'static dyn Fn(&dyn Any) -> Box<dyn Any>)>);
+
+
 
 #[derive(Default)]
 pub struct AttributesBuilder {
   pub attrs: Vec<AttrMetaBuilder<DynLookup>>,
 }
 
+#[derive(Clone)]
 pub enum AttrMetaBuilder<State> {
-  Path(Path, Box<dyn Fn(&mut State)>),
-  List(Path, Box<dyn Fn(&mut State, &dyn Iterator<Item = TokenStream>)>),
-  NameValue(Path, Box<dyn Fn(&mut State, &Expr)>),
-}
-
-pub trait DynLookupExt {
-  type ErrT;
-
-  fn lookup<'a, T: Any>(&'a self, at: impl ToString) -> Result<&'a T, Self::ErrT>;
-  fn lookup_mut<'a, T: Any>(&'a mut self, at: impl ToString) -> Result<&'a mut T, Self::ErrT>;
+  Path(Path, Rc<dyn Fn(&mut State) -> syn::Result<()>>),
+  List(Path, Rc<dyn Fn(&mut State, Box<dyn Iterator<Item = TokenStream>>) -> syn::Result<()>>),
+  NameValue(Path, Rc<dyn Fn(&mut State, &Expr) -> syn::Result<()>>),
 }
 
 pub enum LookupErr {
@@ -34,38 +30,52 @@ impl AttributesBuilder {
     Self { attrs: Vec::new() }
   }
 
-  pub fn update(&self, lookup: &mut DynLookup, attrs: &Vec<Attribute>) {
+  pub fn update(&self, lookup: &mut DynLookup, attrs: &Vec<Attribute>) -> syn::Result<()> {
     for attr in &self.attrs {
-      attr.update_state(lookup, attrs)
+      attr.update_state(lookup, attrs)?
     }
+    Ok(())
   }
 
-  pub fn generate_lookup(&self, attrs: &Vec<Attribute>) -> DynLookup {
+  pub fn generate_lookup(&self, attrs: &Vec<Attribute>) -> syn::Result<DynLookup> {
     let mut res = DynLookup::new();
-    self.update(&mut res, attrs);
-    res
+    self.update(&mut res, attrs)?;
+    Ok(res)
   }
 }
 
-impl DynLookupExt for DynLookup {
-  type ErrT = LookupErr;
+impl DynLookup {
+  pub fn new() -> Self {
+    Self(HashMap::new())
+  }
 
-  fn lookup<'a, T: Any>(&'a self, at: impl ToString) -> Result<&'a T, Self::ErrT> {
-    match self.get(&at.to_string()) {
-      Some(val) => match val.downcast_ref::<T>() {
+  pub fn insert<T: Clone + Any>(&mut self, k: String, v: T) -> Option<Box<dyn Any>> {
+    fn clone<T: Clone + Any>(x: &dyn Any) -> Box<dyn Any> {
+      Box::new(x.downcast_ref::<T>().unwrap().clone())
+    }
+    let v: (Box<dyn Any>, &'static dyn Fn(&dyn Any) -> Box<dyn Any>) = (Box::new(v), &clone::<T>);
+    match self.0.insert(k, v) {
+      Some((v, _)) => Some(v),
+      None => None,
+    }
+  }
+
+  pub fn get<'a, T: Any + Clone>(&'a self, at: impl ToString) -> Result<&'a T, LookupErr> {
+    match self.0.get(&at.to_string()) {
+      Some((val, _)) => match val.downcast_ref::<T>() {
         Some(val) => Ok(val),
         None => Err(LookupErr::WrongTypeCast),
-      },
+      }
       None => Err(LookupErr::KeyNotFound),
     }
   }
 
-  fn lookup_mut<'a, T: Any>(&'a mut self, at: impl ToString) -> Result<&'a mut T, Self::ErrT> {
-    match self.get_mut(&at.to_string()) {
-      Some(val) => match val.downcast_mut::<T>() {
+  pub fn get_mut<'a, T: Any + Clone>(&'a mut self, at: impl ToString) -> Result<&'a mut T, LookupErr> {
+    match self.0.get_mut(&at.to_string()) {
+      Some((val, _)) => match val.downcast_mut::<T>() {
         Some(val) => Ok(val),
         None => Err(LookupErr::WrongTypeCast),
-      },
+      }
       None => Err(LookupErr::KeyNotFound),
     }
   }
@@ -73,7 +83,7 @@ impl DynLookupExt for DynLookup {
 
 
 impl<S> AttrMetaBuilder<S> {
-  pub fn update_state<'a>(&self, state: &mut S, attrs: impl IntoIterator<Item = &'a Attribute>) {
+  pub fn update_state<'a>(&self, state: &mut S, attrs: impl IntoIterator<Item = &'a Attribute>) -> syn::Result<()> {
     fn cmp(p1: &Path, p2: &Path) -> bool {
       if p1.segments.len() != p2.segments.len() {
         return false;
@@ -90,18 +100,19 @@ impl<S> AttrMetaBuilder<S> {
         continue;
       }
       match (self, &attr.meta) {
-        (AttrMetaBuilder::Path(_, f), Meta::Path(_)) => f(state),
+        (AttrMetaBuilder::Path(_, f), Meta::Path(_)) => f(state)?,
         (AttrMetaBuilder::List(_, f), Meta::List(xs)) => {
           let tokens = &xs.tokens;
           let tokens: Punctuated<TokenStream, Token![,]> = parse_quote!(#tokens);
-          f(state, &tokens.into_iter())
+          f(state, Box::new(tokens.into_iter()))?
         },
         (AttrMetaBuilder::NameValue(_, f), Meta::NameValue(x)) => {
-          f(state, &x.value)
+          f(state, &x.value)?
         },
         _ => {}
       }
     }
+    Ok(())
   }
 
   const fn path(&self) -> &Path {
@@ -140,3 +151,21 @@ impl ToString for AttributesBuilder {
 }
 
 
+// impl Deref for DynLookup {
+//   type Target = HashMap<String, Box<dyn Any>>;
+//   fn deref(&self) -> &Self::Target { &self.0 }
+// }
+// impl DerefMut for DynLookup {
+//   fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
+// }
+
+
+impl Clone for DynLookup {
+  fn clone(&self) -> Self {
+    let inner = self.0.iter().map(|(k, (v, clone))| {
+      let v = clone(v);
+      (k.clone(), (v, *clone))
+    }).collect();
+    Self(inner)
+  }
+}
--- crates/proc-builder/src/trait_derivation_builder.rs
+++ crates/proc-builder/src/trait_derivation_builder.rs
@@ -1,32 +1,72 @@
-use std::rc::Rc;
+use std::{rc::Rc, str::FromStr};
 
-use syn::{DeriveInput, ItemFn, Attribute};
+use quote::{ToTokens, quote};
+use syn::{DeriveInput, ItemFn, Attribute, Ident};
 
-use crate::{AttributesBuilder, FnBuilder, DynLookup};
+use crate::{AttributesBuilder, FnBuilder, DynLookup, AttrMetaBuilder};
 
 pub struct TraitDerivationBuilder {
+  pub ident: Ident,
   pub attributes: AttributesBuilder,
   pub functions: Vec<FnBuilder>,
   pub apply_lookup: Rc<dyn Fn(&DynLookup, &mut Vec<FnBuilder>)>,
 }
 
 pub struct TraitDerivation<'a> {
+  pub ident: Ident,
   pub input: &'a DeriveInput,
   pub functions: Vec<ItemFn>,
 }
 
 impl TraitDerivationBuilder {
-  pub fn new() -> Self {
+  pub fn new(ident: Ident) -> Self {
     fn noop(_: &DynLookup, _: &mut Vec<FnBuilder>) {}
     Self {
+      ident,
       attributes: AttributesBuilder::new(),
       functions: Vec::new(),
       apply_lookup: Rc::new(noop),
     }
   }
 
-  pub fn populate_lookup(&self, lookup: &mut DynLookup, attrs: &Vec<Attribute>) {
-    self.attributes.update(lookup, attrs);
+
+  pub fn override_ident(mut self, ident: Ident) -> Self {
+    self.ident = ident;
+    self
+  }
+
+  pub fn set_ident(self, ident: impl ToString) -> Self {
+    self.override_ident(syn::parse_str(ident.to_string().as_str()).unwrap())
+  }
+
+  pub fn push_attribute(self, attr: AttrMetaBuilder<DynLookup>) -> Self {
+    self.push_attributes(Some(attr).into_iter())
+  }
+  pub fn push_attributes(mut self, attrs: impl Iterator<Item = AttrMetaBuilder<DynLookup>>) -> Self {
+    for a in attrs {
+      self.attributes.attrs.push(a);
+    }
+    self
+  }
+
+  pub fn push_function(self, f: FnBuilder) -> Self {
+    self.push_functions(Some(f).into_iter())
+  }
+  pub fn push_functions(mut self, fs: impl Iterator<Item = FnBuilder>) -> Self {
+    for f in fs {
+      self.functions.push(f);
+    }
+    self
+  }
+
+  pub fn set_lookup_action(mut self, f: impl Fn(&DynLookup, &mut Vec<FnBuilder>) + 'static) -> Self {
+    self.apply_lookup = Rc::new(f);
+    self
+  }
+
+
+  pub fn populate_lookup(&self, lookup: &mut DynLookup, attrs: &Vec<Attribute>) -> syn::Result<()> {
+    self.attributes.update(lookup, attrs)
   }
 
   pub fn function_builders(&self, lookup: &DynLookup) -> Vec<FnBuilder> {
@@ -40,23 +80,53 @@ impl TraitDerivationBuilder {
   }
 
   pub fn derive_impl<'a>(&self, lookup: &DynLookup, input: &'a DeriveInput) -> TraitDerivation<'a> {
+    let ident = self.ident.clone();
     let functions = self.functions(lookup);
-    TraitDerivation { input, functions }
+    TraitDerivation { ident, input, functions }
   }
 
-  pub fn derive_impls_with_lookup<'a>(builders: Vec<&Self>, input: &'a DeriveInput, lookup: &mut DynLookup) -> Vec<TraitDerivation<'a>> {
+  pub fn derive_impls_with_lookup<'a>(builders: Vec<&Self>, input: &'a DeriveInput, lookup: &mut DynLookup) -> syn::Result<Vec<TraitDerivation<'a>>> {
     for b in &builders {
-      b.populate_lookup(lookup, &input.attrs)
+      b.populate_lookup(lookup, &input.attrs)?
     }
-    builders.iter().map(|b| b.derive_impl(&lookup, input)).collect()
+    let res = builders.iter().map(|b| b.derive_impl(&lookup, input)).collect();
+    Ok(res)
   }
 
-  pub fn derive_impls<'a>(builders: Vec<&Self>, input: &'a DeriveInput) -> (Vec<TraitDerivation<'a>>, DynLookup) {
+  pub fn derive_impls<'a>(builders: Vec<&Self>, input: &'a DeriveInput) -> syn::Result<(Vec<TraitDerivation<'a>>, DynLookup)> {
     let mut lookup = DynLookup::new();
-    let traits = Self::derive_impls_with_lookup(builders, input, &mut lookup);
-    (traits, lookup)
+    let traits = Self::derive_impls_with_lookup(builders, input, &mut lookup)?;
+    Ok((traits, lookup))
   }
 }
 
 
+impl FromStr for TraitDerivationBuilder {
+  type Err = syn::Error;
 
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let ident = syn::parse_str(s)?;
+    Ok(Self::new(ident))
+  }
+}
+
+
+impl ToTokens for TraitDerivation<'_> {
+  fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+    let t_ident = &self.ident;
+    let ident = &self.input.ident;
+    let (impl_gen, ty_gen, where_cl) = self.input.generics.split_for_impl();
+    let mut fns = proc_macro2::TokenStream::new();
+    for f in &self.functions {
+      f.to_tokens(&mut fns)
+    }
+    tokens.extend(quote!(
+      impl #impl_gen #t_ident for #ident #ty_gen #where_cl {
+        #fns
+      }
+    ));
+    
+  }
+}
+
+// impl syn::TraitItem
